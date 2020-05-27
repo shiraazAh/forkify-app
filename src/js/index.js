2b813d5444984923c624a9509f3030641ceddf48
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-05-27 12:25:21 +0530

    Implemented a Structure for the controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,29 @@
-import axios from 'axios'; 
+import Search from './models/Search';
+
+/*** Global state of app ***/
+/*  -Search Object
+    -Current Recipe Object
+    -Shopping List object
+    -Liked Object
+*/
+
+const state = {}
+
+const controlSearch = async () => {
+    // 1) Get query from view 
+    const query = 'pizza' //TODO
+
+    if (query) {
+        // 2) Create new search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI For Results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI 
+        console.log(state.search.result)
+    }
+} 
+
