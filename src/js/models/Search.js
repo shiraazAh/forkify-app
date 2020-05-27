a4eb6b36cbec42e69fbb94373a5ee372bc325869
Shiraaz Ahammed <shiraaz.ahammed@gmail.com>
2020-05-27 12:27:45 +0530

    Addition: Created a search model for search controller

--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,17 @@
-export default "hi this is a string";
+import axios from 'axios'; 
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        try {
+        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+        this.result = res.data.recipes;
+        console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
